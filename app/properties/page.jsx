18cf02863370fe7342ce405a173ac3b37a268454
ohziiilov3 <klbaskerville0520@gmail.com>
2025-03-20 22:19:02 -0700

    add: properties page and property cards, dynamically rendering data

--- app/properties/page.jsx
+++ app/properties/page.jsx
@@ -1,6 +1,22 @@
+import PropertyCard from '@/components/PropertyCard';
+import propertiesData from '@/properties.json'
+
 const PropertiesPage = () => {
+    // console.log(propertiesData);
     return ( 
-        <div>Properties Page</div>
+        <section className='px-4 py-6'>
+            <div className="container-xl lg:container m-auto px-4 py-6">
+                {propertiesData.length === 0 ? (
+                    <p>No properties found</p>
+                ) : (
+                    <div className='grid grid-cols-1 md:grid-cols-3 gap-6'>
+                        {propertiesData.map((property) =>(
+                                <PropertyCard key={property._id} property={property} />
+                            ))}
+                    </div>
+                )}
+            </div>
+        </section>
      );
 }
  
