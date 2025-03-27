45c2662bdf60d9dc49fb911d7cc98e437279eb5e
ohziiilov3 <klbaskerville0520@gmail.com>
2025-03-26 18:09:16 -0700

    add: save user to db & session

--- utlis/authOptions.js
+++ utlis/authOptions.js
@@ -1,31 +1,49 @@
 import GoogleProvider from "next-auth/providers/google";
+import connectDB from "@/config/database";
+import User from "@/models/User";
 
 export const authOptions = {
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SERCET,
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-    }),
-  ],
+    providers: [
+        GoogleProvider({
+          clientId: process.env.GOOGLE_CLIENT_ID,
+          clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+          authorization: {
+            params: {
+              prompt: "consent",
+              access_type: "offline",
+              response_type: "code"
+            }
+          }
+        })
+      ],
   callbacks: {
     // invoked on successful dign in
     async signIn({ profile }) {
       // 1 connect to db
+      await connectDB();
       // 2 check if user exists
+      const userExists = await User.findOne({ email: profile.email });
       // 3 if not create user
+      if (!userExists) {
+        // truncate username if too long
+        const username = profile.name.slice(0, 20);
+
+        await User.create({
+          email: profile.email,
+          username,
+          image: profile.picture,
+        });
+      }
+      return true;
       // 4 return true to allow sign in
     },
     //Session callback function that modifies the session object
     async session({ session }) {
       // 1 Get user from db
+      const user = await User.findOne({ email: session.user.email });
       // 2 assign user id from the session
+      session.user.id = user._id.toString();
+      return session;
       // return the session
     },
   },
