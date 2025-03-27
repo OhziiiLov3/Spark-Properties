793d04c44b4f8972b435bc30417812a582dcd303
ohziiilov3 <klbaskerville0520@gmail.com>
2025-03-27 10:35:58 -0700

    add: ProperyAddForm component

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,15 @@ import '@/assets/styles/globals.css';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import AuthProvider from '@/components/AuthProvider';
+import { Poppins } from 'next/font/google';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  weight: ['500', '700'], 
+  variable: 'fontFamily',
+});
+
 
 export const metadata = {
   title: 'Spark Properties',
@@ -16,7 +25,7 @@ export const metadata = {
 const MainLayout = ({children}) => {
   return (
     <AuthProvider>
-    <html suppressHydrationWarning={true}>
+    <html suppressHydrationWarning={true} className={poppins.className}>
       <body>
         <Navbar/>
         <main>{ children }</main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,19 @@
 // tailwind.config.js
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
-        fontFamily:{
-           sans: ['Poppins','sans-serif'],
-        },
-        gridTemplateColumns:{
-            '70/30' :'70% 28%',
-        } ,
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif'],
+      },
+      gridTemplateColumns: {
+        '70/30': '70% 28%',
+      },
     },
   },
   plugins: [],
