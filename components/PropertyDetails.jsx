434bf49a77affc9e71a7ea8b5a60dd9e5eca7ffd
ohziiilov3 <klbaskerville0520@gmail.com>
2025-04-10 14:17:15 -0700

    add:Geocode for lat and lng

--- components/PropertyDetails.jsx
+++ components/PropertyDetails.jsx
@@ -1,5 +1,5 @@
 import { FaTimes, FaBed, FaBath, FaRulerCombined, FaCheck, FaMapMarker } from "react-icons/fa";
-
+import PropertyMap from '@/components/PropertyMap';
 
 
 const PropertyDetails = ({property}) => {
@@ -94,8 +94,10 @@ const PropertyDetails = ({property}) => {
             </div>
        
             <div className="bg-white p-6 rounded-lg shadow-md mt-6">
-              <div id="map"></div>
-              Map
+              <div id="map">
+                <PropertyMap property={property}/>
+              </div>
+           
             </div>
           </main>
      );
