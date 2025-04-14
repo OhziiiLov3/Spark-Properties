1109e671f1a31e526f593f3f234947563909c39b
ohziiilov3 <klbaskerville0520@gmail.com>
2025-04-14 14:35:49 -0700

    add:Photoswipe Ligthbox modal

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,11 +1,15 @@
-import '@/assets/styles/globals.css';
 import Footer from '@/components/Footer';
 import Navbar from '@/components/Navbar';
 import AuthProvider from '@/components/AuthProvider';
 import {GlobalProvider} from '@/context/GlobalContext';
 import { Poppins } from 'next/font/google';
 import {ToastContainer} from 'react-toastify';
+import '@/assets/styles/globals.css';
 import 'react-toastify/dist/ReactToastify.css';
+import 'photoswipe/dist/photoswipe.css'
+
+
+
 
 const poppins = Poppins({
   subsets: ['latin'],
